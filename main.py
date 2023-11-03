c03d4ef5b21fe89e0034b7422dc25dfec647a90f
Fabrizzio <fabrizizocespedes@gmail.com>
2023-11-02 21:36:54 -0300

    ok

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for, redirect, flash
+from flask import Flask, flash, render_template, request, session, url_for, redirect, flash
 from werkzeug.security import check_password_hash
 import psycopg2
 
@@ -20,6 +20,7 @@ conn = psycopg2.connect(
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    msg = ''
     if request.method == 'POST' and 'usuario' in request.form and 'contra' in request.form:
         usuario = request.form['usuario']
         contra = request.form['contra']
@@ -27,26 +28,23 @@ def login():
         cur.execute("SELECT * FROM users WHERE usuario = %s AND contra = %s", (usuario, contra))
         users = cur.fetchone()
         if users:
-            session['loggedin'] = True
-            session['id'] = users['id_user']
-            session['usuario'] = users['usuario']  # Almacena el ID del usuario en la sesión
+            #session['loggedin'] = True
+            session['iduser'] = users[0]
+            session['usuario'] = users[0]  # Almacena el ID del usuario en la sesión
             return render_template('auth/index.html')
         else:
-            return "Credenciales incorrectas. Inténtalo de nuevo."
+            msg = "Credenciales incorrectas. Inténtalo de nuevo."
 
-    return render_template('auth/login.html')  # Debes crear una plantilla HTML para el formulario de inicio de sesión
+    return render_template('auth/login.html', msg = msg)  # Debes crear una plantilla HTML para el formulario de inicio de sesión
 
 
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None) 
-    session.pop('id_user', None) 
+    session.pop('iduser', None) 
     session.pop('usuario', None)  # Elimina la información de sesión del usuario
     return redirect(url_for('login'))
 
-@app.route("/")
-def lo():
-    return render_template('auth/login')
 
 if __name__ == '__main__':
   app.run(debug=True, port=81)
