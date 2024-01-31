d98484235e20fcfd58cd7728ca308bb7bd7b58f7
Fabrizzio <fabrizizocespedes@gmail.com>
2024-01-30 23:22:50 -0300

    updated

--- app/dao/referenciales/ciudad/CiudadDao.py
+++ app/dao/referenciales/ciudad/CiudadDao.py
@@ -21,7 +21,7 @@ class CiudadDao:
             cur.close()
             con.close()
             
-    def getCiudadById(self, id):
+    def getCiudadById(self, idciudad):
         
         ciudadSQL = """
             SELECT idciudad, nom_city, abr_city
@@ -31,7 +31,7 @@ class CiudadDao:
         con = conexion.getConexion()
         cur = con.cursor()
         try:    
-            cur.execute(ciudadSQL, (id,))
+            cur.execute(ciudadSQL, (idciudad,))
             ciudad = cur.fetchone()
             if ciudad:
                 return { 'idciudad': ciudad[0], 'nom_city': ciudad[1], 'abr_city': ciudad[2]}
@@ -43,7 +43,7 @@ class CiudadDao:
             cur.close()
             con.close()
             
-    def insertCiudad(self, descripcion):
+    def insertCiudad(self, nom_city, abr_city):
         
         insertSQL = """
             INSERT INTO ciudades(nom_city, abr_city) VALUES(%s)
@@ -52,7 +52,7 @@ class CiudadDao:
         con = conexion.getConexion()
         cur = con.cursor()
         try:    
-            cur.execute(insertSQL, (descripcion,))
+            cur.execute(insertSQL, (nom_city, abr_city))
             con.commit()
             return True
         except con.Error as e:
@@ -63,7 +63,7 @@ class CiudadDao:
             con.close()
         return False
     
-    def updateCiudad(self, id, descripcion):
+    def updateCiudad(self, id, nom_city, abr_city):
         
         updateSQL = """
             UPDATE ciudades SET nom_city = %s and abr_city = %s WHERE idciudad = %s
@@ -72,7 +72,7 @@ class CiudadDao:
         con = conexion.getConexion()
         cur = con.cursor()
         try:    
-            cur.execute(updateSQL, (descripcion, id,))
+            cur.execute(updateSQL, (nom_city, abr_city, id,))
             con.commit()
             return True
         except con.Error as e:
@@ -83,7 +83,7 @@ class CiudadDao:
             con.close()
         return False
     
-    def deleteCiudad(self, id):
+    def deleteCiudad(self, idciudad):
         
         deleteSQL = """
             DELETE FROM ciudades WHERE idciudad = %s
@@ -92,7 +92,7 @@ class CiudadDao:
         con = conexion.getConexion()
         cur = con.cursor()
         try:    
-            cur.execute(deleteSQL, (id,))
+            cur.execute(deleteSQL, (idciudad,))
             con.commit()
             return True
         except con.Error as e:
